96a78b6b8e0cd5cca308940ebb9fa3a52b6105cd
Azat <azat.takhautdinov@gmail.com>
2022-12-02 23:06:21 +0500

    game in rock paper scissors with a bot

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        String Check = scan.nextLine();
+
+        List<String> game = new ArrayList<>();
+        game.add("Камень");
+        game.add("Ножницы");
+        game.add("Бумага");
+
+        Random random = new Random();
+        
+
+
+    }
+}
