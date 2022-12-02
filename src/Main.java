be09970188c56361dfe765b0f6b45d434b388c25
Azat <azat.takhautdinov@gmail.com>
2022-12-02 23:38:49 +0500

    don't fixed

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,59 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Напиши одно из трёх: Камень, Ножницы, Бумага");
         Scanner scan = new Scanner (System.in);
         String Check = scan.nextLine();
 
-        List<String> game = new ArrayList<>();
+        List<String> game = new LinkedList<>();
         game.add("Камень");
         game.add("Ножницы");
         game.add("Бумага");
 
         Random random = new Random();
-        
+        int i = random.nextInt(3);
+        String randS = game.get(i);
+        System.out.println("Бот выбрал "+randS);
 
+        if (randS.equals("Камень") && Check.equals("Камень")){
+            System.out.println("Ничья");
+        }
+        if (randS=="Ножницы"&&Check=="Ножницы"){
+            System.out.println("Ничья");
+        }
+        if (randS=="Бумага"&&Check=="Бумага"){
+            System.out.println("Ничья");
+        }
+        if (randS=="Камень"&&Check=="Ножницы"){
+            System.out.println("Победил бот");
+        }
+        if (randS=="Камень"&&Check=="Бумага"){
+            System.out.println("Победил игрок");
+        }
+        if (randS=="Ножницы"&&Check=="Бумага"){
+            System.out.println("Победил бот");
+        }
+        if (randS=="Ножницы"&&Check=="Камень"){
+            System.out.println("Победил игрок");
+        }
+        if (randS=="Бумага"&&Check=="Камень"){
+            System.out.println("Победил бот");
+        }
+        if (randS=="Бумага"&&Check=="Ножницы"){
+            System.out.println("Победил игрок");
+        }
+
+//        switch (Check) {
+//            case "Камень":
+//                if
+//        }
+
+
+
+
+
+        System.out.println(randS);
 
     }
 }
